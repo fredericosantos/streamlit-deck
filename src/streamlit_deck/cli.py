b3d1990654ef03d5f96565996860ad0b496b2f37
frederico santos <fredericobsantos@gmail.com>
2026-02-11 20:28:41 +0000

    Inject theme configuration in CLI entry point

--- src/streamlit_deck/cli.py
+++ src/streamlit_deck/cli.py
@@ -9,6 +9,17 @@ def run():
 
     # Construct the command for streamlit
     # We set argv as if we called "streamlit run file.py"
-    sys.argv = ["streamlit", "run", filename]
+    sys.argv = [
+        "streamlit",
+        "run",
+        filename,
+        "--theme.base=dark",
+        "--theme.primaryColor=#ff0000",
+        "--theme.backgroundColor=#000000",
+        "--theme.secondaryBackgroundColor=#111111",
+        "--theme.textColor=#ffffff",
+        "--theme.borderColor=#333333",
+        "--theme.font='IBM Plex Mono':https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:wght@300;400;500;600;700&display=swap",
+    ]
 
     sys.exit(stcli.main())
